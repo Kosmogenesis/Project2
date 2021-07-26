0dba8ef9c6bc189c5aea5608d824922bb852bc0a
unknown <michael.carson80@yahoo.com>
2021-07-25 23:17:18 -0400

    created very basic front end

--- P2Angular/Project2/src/app/app-routing.module.ts
+++ P2Angular/Project2/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ {
+   path:'welcome',
+   component:WelcomeComponent
+  },
+
+  {
+   path:'', //the empty String makes this component appear given the base URL (on launch)
+   component:WelcomeComponent //so when I go to localhost:4200, we'll get this component
+  },
+
+  {
+   path:'aboutus', //the empty String makes this component appear given the base URL (on launch)
+   component:AboutusComponent //so when I go to localhost:4200, we'll get this component
+  },
+
+  {
+   path:'login', //the empty String makes this component appear given the base URL (on launch)
+   component:LoginComponent //so when I go to localhost:4200, we'll get this component
+  },
+
+  {
+   path:'signup', //the empty String makes this component appear given the base URL (on launch)
+   component:SignupComponent //so when I go to localhost:4200, we'll get this component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- P2Angular/Project2/src/app/app.module.ts
+++ P2Angular/Project2/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    AboutusComponent
+   
   ],
   imports: [
     BrowserModule,
